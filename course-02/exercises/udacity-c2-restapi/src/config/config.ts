603d6c539b222fd5dc75a93b99decc74a853f1cb
Tunmbi Banto <hellotunmbi@gmail.com>
2022-08-29 10:02:50 +0100

    relocated screenshot img

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,22 +1,23 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: "",
+    password: "",
+    database: "",
+    host: "",
+    dialect: "postgres",
+    aws_region: "us-east-2",
+    aws_profile: "default",
+    // "aws_media_bucket": "udagram-ruttner-dev"
+    aws_media_bucket: "my-tunmbi-bucket",
   },
-  "jwt": {
-    "secret": " "
+  jwt: {
+    secret: " ",
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+  },
+};
